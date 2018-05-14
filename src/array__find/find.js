4434da8f25ed9f598ddfcc89f32775c23ab2c3f4
Andrei <andrei@codemachiner.com>
2018-05-14 09:24:34 +0200

    Add "array__find" function

---
+++ src/array__find/find.js
@@ -0,0 +1,23 @@
+/**
+ * Find the first element that satisfies the matchFn
+ *
+ * @param  {Function}    matchFn Function applied to each element
+ * @param  {Array}       source  Source array
+ *
+ * @return {mixed|undefined}  First element
+ *
+ * @example
+ *
+ */
+module.exports = matchFn => source => {
+  for ( let i = 0,length = source.length; i < length; i++ ) {
+    const found = matchFn( source[ i ], i, source )
+
+    if ( found === true ) {
+      return source[ i ]
+    }
+  }
+
+  return undefined
+}
+
---
+++ src/array__find/find.test.js
@@ -0,0 +1,31 @@
+const test = require( "tape" )
+const find = require( "./find" )
+
+/**
+ * Find the first element that satisfies the matchFn
+ *
+ * @param  {Function}    matchFn Function applied to each element
+ * @param  {Array}       source  Source array
+ *
+ * @return {mixed|undefined}  First element
+ *
+ * @example
+ *
+ */test( "array::find( matchFn: Function )( source: Array ): mixed", t => {
+  const comments = [
+    { id: 1, body: "" },
+    { id: 2, body: "dolor" },
+  ]
+
+  t.deepEqual(
+    find( element => element.id === 2 )( comments ),
+    { id: 2, body: "dolor" },
+    "find with id:2 should return found object" )
+
+  t.equal(
+    find( element => element.id === 3 )( comments ),
+    undefined,
+    "find with id:3 should return undefined (not found)" )
+
+  t.end()
+} )
