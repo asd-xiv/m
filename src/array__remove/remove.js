5179e07703926a090667a8894e65aaf059b1f735
Andrei Dumitrescu <andrei@codemachiner.com>
2018-10-24 16:34:43 +0200

    Write tests for and update `array__remove` to accept primitive value or filter function

--- src/array__remove/remove.js
+++ src/array__remove/remove.js
@@ -1,11 +1,31 @@
-const filter = require("../array__filter/filter")
-
 /**
- * { lambda_description }
+ * Remove element(s) from array by value or by filter function
  *
- * @param  {mixed}  value  Value to remove
- * @param  {Array}  array  Array from where to remove
+ * @param  {Function|mixed}  value   Value to remove
+ * @param  {Array}           source  Source array
  *
  * @return {Array}
+ *
+ * @tag Array
+ * @signature (value: Function|mixed) => (source: Array): Array
+ *
+ * @example
+ * remove(3)([1, 2, 3])
+ * // => [1, 2]
+ * remove(_ => _ === 3)([1, 2, 3])
+ * // => [1, 2]
  */
-module.exports = value => filter(element => element !== value)
+module.exports = value => source => {
+  const result = []
+
+  for (let i = 0, length = source.length; i < length; i++) {
+    const isMatch =
+      typeof value === "function"
+        ? value.call(null, source[i]) === true
+        : value === source[i]
+
+    !isMatch && result.push(source[i])
+  }
+
+  return result
+}
---
+++ src/array__remove/remove.test.js
@@ -0,0 +1,39 @@
+const test = require("tape")
+const remove = require("./remove")
+
+/**
+ * Remove element(s) from array by value or by filter function
+ *
+ * @param  {Function|mixed}  value   Value to remove
+ * @param  {Array}           source  Source array
+ *
+ * @return {Array}
+ *
+ * @tag Array
+ * @signature (value: Function|mixed) => (source: Array): Array
+ *
+ * @example
+ * remove(3)([1, 2, 3])
+ * // => [1, 2]
+ * remove(_ => _ === 3)([1, 2, 3])
+ * // => [1, 2]
+ */
+test("array::remove", t => {
+  t.deepEqual(
+    remove(_ => _ === 3)([1, 2, 3]),
+    [1, 2],
+    "Remove existing element from array by match function"
+  )
+
+  t.deepEqual(
+    remove(3)([1, 2, 3]),
+    [1, 2],
+    "Remove existing element from array by value"
+  )
+
+  const source = [1, 2, 3]
+
+  t.notEqual(remove(3)(source), source, "Imutable")
+
+  t.end()
+})
