d309ba29e326b85face33edb72a233f53cfbd2b9
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-01-05 18:23:19 +0100

    Add "findIndex" - Find the position of first element that satisfies a function

---
+++ src/find-index/find-index.js
@@ -0,0 +1,31 @@
+/**
+ * Find the position of first element that satisfies a function
+ *
+ * @param  {Function}    fn      Match function
+ * @param  {Object[]}    source  Source array
+ *
+ * @return {number}
+ *
+ * @name findIndex
+ * @tag Array
+ * @signature (fn: Function) => (source: Object[]): number
+ *
+ * @example
+ * const comments = [{ id: 1, body: "" }, { id: 2, body: "dolor" }]
+ *
+ * findIndex(item => item.id === 2)(comments)
+ * // => 1
+ * findIndex(item => item.id === 3)(comments)
+ * // => -1
+ */
+module.exports = fn => source => {
+  for (let i = 0, length = source.length; i < length; i++) {
+    const found = fn(source[i], i, source)
+
+    if (found === true) {
+      return i
+    }
+  }
+
+  return -1
+}
