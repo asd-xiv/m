878158ccc2983d8773ca657b623ac9860dd4ae16
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-06-08 11:23:42 +0200

    Partially apply a function

---
+++ src/curry/curry.js
@@ -0,0 +1,24 @@
+/**
+ * Partially apply a function
+ *
+ * @signature (...args: mixed[]): Function|mixed
+ *
+ * @param {Function}  fn    The function to apply
+ * @param {any}       args  The arguments to apply, in order
+ *
+ * @return  {Function|mixed}  If the number of arguments provided is sufficient
+ * to call the function, call the function and return the result. Otherwise,
+ * return a new function which takes additional parameters, returning the result
+ * of calling `curry` on the function with the provided parameters.
+ *
+ * @example
+ * const sum = (a, b) => a + b
+ *
+ * curry(sum)(1)(2) = 3
+ */
+const curry = fn => (...args) =>
+  args.length >= fn.length
+    ? fn(...args)
+    : (...rest) => curry(fn)(...args, ...rest)
+
+export { curry }
---
+++ src/curry/curry.test.js
@@ -0,0 +1,26 @@
+import test from "tape"
+import { curry } from ".."
+
+test("curry", t => {
+  const sum = (a, b) => a + b
+
+  // does nothing if there are enough parameters
+  t.equal(curry(sum)(2, 3), 5)
+
+  // curries the function if there aren't
+  const addTwo = curry(sum)(2)
+
+  t.equal(addTwo(3), 5)
+
+  // can curry multiple times and pass multiple parameters on each curry
+  const sumThree = (a, b, c) => a + b + c
+
+  const addOne = curry(sumThree)(1)
+  const addFourA = curry(addOne)(3)
+  const addFourB = curry(sumThree)(2, 2)
+
+  t.equal(addFourA(1), 5)
+  t.equal(addFourA(1), addFourB(1))
+
+  t.end()
+})
