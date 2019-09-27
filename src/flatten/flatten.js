46b832d701b1294260090cf77f4a133c1b18bbd9
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2019-09-27 14:07:40 +0200

    feat: Add function to flatten nested object

--- src/flatten/flatten.js
+++ src/flatten/flatten.js
@@ -1,20 +1,52 @@
 import { reduce } from "../reduce/reduce"
+import { is, isObject } from "../is/is"
+
+const flattenArray = reduce(
+  (acc, item) =>
+    Array.isArray(item) ? [...acc, ...flattenArray(item)] : [...acc, item],
+  []
+)
+
+const flattenObject = (source, prefix = null) =>
+  reduce((acc, [key, value]) => {
+    return isObject(value)
+      ? {
+          ...acc,
+          ...flattenObject(value, is(prefix) ? `${prefix}__${key}` : key),
+        }
+      : {
+          ...acc,
+          [is(prefix) ? `${prefix}__${key}` : key]: value,
+        }
+  }, {})(Object.entries(source))
 
 /**
  * Recursively concat all arrays intro a single array
  *
- * @param  {Array}  input  Array with nested arrays
+ * @param  {Array|Object}  source  Array or Object to flatten
  *
  * @return {Array}  1 level deep array
  *
  * @example
- * flatten( [ 1, [ 2 ], [ 3, [ 4 ] ] ] )
- * // => [ 1, 2, 3, 4 ]
+ * flatten([1, [2], [3, [4]]])
+ * // => [1, 2, 3, 4]
+ *
+ * flatten({test: {a: 1, b: {c: 2}}})
+ * // => {
+ *  test__a: 1,
+ *  test__b__c: 2
+ * }
  */
-const flatten = reduce(
-  (acc, item) =>
-    Array.isArray(item) ? [...acc, ...flatten(item)] : [...acc, item],
-  []
-)
+const flatten = source => {
+  if (Array.isArray(source)) {
+    return flattenArray(source)
+  }
+
+  if (typeof source === "object") {
+    return flattenObject(source)
+  }
+
+  return source
+}
 
 export { flatten }
--- src/flatten/flatten.test.js
+++ src/flatten/flatten.test.js
@@ -3,12 +3,34 @@ import { flatten } from ".."
 
 test("flatten", t => {
   t.deepEqual(flatten([]), [], "[] should equal []")
-  t.deepEqual(flatten([[[]]]), [], "[ [ [] ] ] should equal []")
-  t.deepEqual(flatten([[[1]]]), [1], "[ [ [1] ] ] should equal [ 1 ]")
+  t.deepEqual(flatten([[[]]]), [], "[[[]]] should equal []")
+  t.deepEqual(flatten([[[1]]]), [1], "[[[1]]] should equal [1]")
   t.deepEqual(
     flatten([1, [2], [3, [4]]]),
     [1, 2, 3, 4],
-    "[ 1, [2], [3, [4]], [] ] should equal [ 1, 2, 3, 4 ]"
+    "[1, [2], [3, [4]], []] should equal [1, 2, 3, 4]"
+  )
+
+  t.deepEqual(
+    flatten({
+      test: {
+        inside: "one",
+        another: "two",
+        lvl: {
+          three: "3",
+        },
+      },
+      lorem: "ipsum",
+      an_array: [1, 2],
+    }),
+    {
+      test__inside: "one",
+      test__another: "two",
+      test__lvl__three: "3",
+      lorem: "ipsum",
+      an_array: [1, 2],
+    },
+    "Nested objects should return one lvl deep object"
   )
 
   t.end()
