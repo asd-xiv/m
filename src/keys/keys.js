c56178e2df9a8d006d16c2e55b7d84a7b240e883
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2020-05-18 23:54:47 +0200

    feat: Add "keys" for getting an Object's property names

---
+++ src/keys/keys.js
@@ -0,0 +1,45 @@
+/**
+ * Get list with names of all own properties
+ *
+ * @tag Array,Object
+ * @signature (source: Array|Object): string[]
+ *
+ * @param {Array|Object} source Array or Object to extract keys from
+ *
+ * @return  {string[]} List of property names
+ *
+ * @example
+ * keys(["lorem", "ipsum"])
+ * // => ["0", "1"]
+ *
+ * keys({ foo: "bar", lorem: "ipsum"})
+ * // => ["foo", "lorem"]
+ *
+ * keys("foo"), keys(12), keys(null), etc
+ * // => []
+ */
+const keys = source => {
+  const type = Array.isArray(source)
+    ? "array"
+    : source !== null && typeof source === "object"
+    ? "object"
+    : "other"
+
+  switch (type) {
+    case "object":
+    case "array": {
+      const entries = Object.entries(source)
+      const result = []
+
+      for (let i = 0; i < entries.length; ++i) {
+        result.push(entries[i][0])
+      }
+
+      return result
+    }
+    default:
+      return []
+  }
+}
+
+export { keys }
---
+++ src/keys/keys.test.js
@@ -0,0 +1,30 @@
+import test from "tape"
+import { keys } from ".."
+
+test("keys", t => {
+  t.deepEquals(
+    keys(["test", 1, 2]),
+    ["0", "1", "2"],
+    "When passing an array, should return an array of string with the index of each element"
+  )
+
+  t.deepEquals(
+    keys({ foo: "bar", lorem: "ipsum" }),
+    ["foo", "lorem"],
+    "When passing an object, should return an array of strings with the key values"
+  )
+
+  t.deepEquals(
+    keys("lorem"),
+    [],
+    "When passing a string, should return an empty array"
+  )
+
+  t.deepEquals(
+    keys(12),
+    [],
+    "when passing a number, should return an empty array"
+  )
+
+  t.end()
+})
