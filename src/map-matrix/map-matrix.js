a12d0f600ee03a8d3d1aede466ad3e9a3bfad912
Andrei Dumitrescu <5057797+andreidcm@users.noreply.github.com>
2020-08-26 22:39:22 +0200

    docs: Update "mapMatrix" comments

--- src/map-matrix/map-matrix.js
+++ src/map-matrix/map-matrix.js
@@ -3,13 +3,15 @@ import { pipe } from "../pipe/pipe"
 const _mapMatrix = (fn, source) => {
   const result = []
 
-  for (let i = 0; i < source.length; ++i) {
+  for (let i = 0, rowCount = source.length; i < rowCount; ++i) {
     const row = []
 
-    for (let j = 0; j < source[i].length; ++j) {
-      const value = source[i][j]
-
-      row.push(Array.isArray(fn) ? pipe(...fn)(value) : fn(value, i, j, source))
+    for (let j = 0, columnCount = source[i].length; j < columnCount; ++j) {
+      row.push(
+        Array.isArray(fn)
+          ? pipe(...fn)(source[i][j])
+          : fn(source[i][j], i, j, source)
+      )
     }
 
     result.push(row)
@@ -19,29 +21,43 @@ const _mapMatrix = (fn, source) => {
 }
 
 /**
- * Two-Dimensional version of Array.map
+ * Matrix version of "map". Iterates over a two-dimensional array and applies
+ * a function on each element, returning a new matrix with the transformed elements.
  *
- * Takes a function and applies it to all elements of the matrix.
+ * @param  {Fn|Fn[]} fn     Transform function called on all elements
+ * @param  {[][]}    source Two-dimensional array to iterate over
  *
- * @param {Function} fn     Function to be called on all elements
- * @param {[][]}     source Matrix to be iterated over
+ * @tag Array
+ * @signature (fn: Fn|Fn[]) => (source: [][]) => [][]
+ * @signature (fn: Fn|Fn[], source: [][]) => [][]
  *
  * @return {[][]} Returns new instance
+ *
+ * @example
+ * const inc = x => x + 1
+ *
+ * mapMatrix(inc, [[1, 2], [3, 4]])
+ * // => [[2, 3], [4, 5]]
+ *
+ * mapMatrix([inc, inc], [[1, 2], [3, 4]])
+ * // => [[3, 4], [5, 6]]
  */
 export const mapMatrix = (...params) => {
   /*
-   * @signature (fn) => (source: [][]): [][]
+   * @signature (fn:Fn|Fn[]) => (source: [][]): [][]
    *
-   * mapMatrix(x => x + 1)([[1, 2], [3, 4]]) => [[2, 3], [4, 5]]
+   * mapMatrix(inc)([[1, 2], [3, 4]])
+   * // => [[2, 3], [4, 5]]
    */
   if (params.length === 1) {
     return source => _mapMatrix(params[0], source)
   }
 
   /*
-   * @signature (fn, source: [][]): [][]
+   * @signature (fn: Fn|Fn[], source: [][]): [][]
    *
-   * mapMatrix(x => x + 1, [[1, 2], [3, 4]]) => [[2, 3], [4, 5]]
+   * mapMatrix(inc, [[1, 2], [3, 4]])
+   * // => [[2, 3], [4, 5]]
    */
   return _mapMatrix(...params)
 }
