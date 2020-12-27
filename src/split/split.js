a1018bc864976307152f551c7fb780499aec6810
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2020-12-27 23:35:38 +0100

    feat: Update "split" to throw type errors on wrong parameters

--- src/split/split.js
+++ src/split/split.js
@@ -1,20 +1,44 @@
+const _split = (separator, source) => {
+  if (!(separator instanceof RegExp) && typeof separator !== "string") {
+    throw new TypeError(
+      `split - invalid 'separator' parameter type '${typeof separator}'. 'separator' type should be a String or RegExp.`
+    )
+  }
+
+  if (typeof source !== "string") {
+    throw new TypeError(
+      `split - invalid 'source' parameter type '${typeof source}'. 'source' type should be String.`
+    )
+  }
+
+  return source.split(separator)
+}
+
 /**
  * Splits a String object into an array of strings by separating the string
  * into substrings, using a specified separator string to determine where to
  * make each split.
  *
  * @tag String
- * @signature ( separator: string|RegExp )( source: string ): Array
+ * @signature (separator: string|RegExp, source:string): Array
+ * @signature (separator: string|RegExp) => (source:string): Array
  *
- * @param {string|RegExp} separator Points where each split should occur
- * @param {string}        source    Source string
+ * @param {...any}        params
+ * @param {string|RegExp} params.separator Points where each split should occur
+ * @param {string}        params.source    Source string
  *
- * @returns  {Array}
+ * @returns {Array}
  *
  * @example
- * split( "," )( "lorem,ipsum" )
- * // [ "lorem", "ipsum" ]
+ * split(",", "lorem,ipsum")
+ * // ["lorem", "ipsum"]
  */
-const split = separator => source => "".split.call(source, separator)
+export const split = (...params) => {
+  // @signature (separator) => (source)
+  if (params.length <= 1) {
+    return source => _split(params[0], source)
+  }
 
-export { split }
+  // @signature (separator, source)
+  return _split(...params)
+}
--- src/split/split.test.js
+++ src/split/split.test.js
@@ -1,14 +1,40 @@
-import test from "tape"
-import { split } from ".."
+/* eslint-disable new-cap */
 
-test("split", t => {
-  const source = "lorem,ipsum"
+import { describe, Try } from "riteway"
+import { split } from "./split"
 
-  t.deepEqual(
-    split(",")(source),
-    ["lorem", "ipsum"],
-    "Split string into an array of 2 substrings"
-  )
+describe("split", async assert => {
+  assert({
+    given: "string that contains one ',', splitting by it",
+    should: "return array with 2 strings",
+    actual: split(",")("lorem,ipsum"),
+    expected: ["lorem", "ipsum"],
+  })
 
-  t.end()
+  assert({
+    given:
+      "string that contains one ',' and one '-', splitting by a RegExp matching both",
+    should: "return array with 3 strings",
+    actual: split(/[,-]/, "foo,bar-lorem"),
+    expected: ["foo", "bar", "lorem"],
+  })
+
+  assert({
+    given: "non-string source",
+    should: "throw",
+    actual: Try(() => {
+      split(",", [1, 2, 3, 4])
+    }).toString(),
+    expected:
+      "TypeError: split - invalid 'source' parameter type 'object'. 'source' type should be String.",
+  })
+
+  assert({
+    given: "non-string, non-regexp separator",
+    should: "throw",
+    actual: Try(() => {
+      split(2, [1, 2, 3, 4])
+    }).toString(),
+    expected: `TypeError: split - invalid 'separator' parameter type 'number'. 'separator' type should be a String or RegExp.`,
+  })
 })
