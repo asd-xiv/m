cb5515691039119a6740229d4eb81d457aee818d
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-02-23 12:06:31 +0100

    feat(debounce): Remove .bind parameter. To keep same behaviour, pass already bound function.
    BREAKING CHANGE:
    
    ```js
    // old
    const handleAutocomplete = debounce(fn, {wait: 50, bind: this})
    
    // new
    const handleAutocomplete = debounce(fn.bind(this), {wait: 50 })
    ```

--- src/debounce/debounce.js
+++ src/debounce/debounce.js
@@ -1,35 +1,33 @@
 /**
  * Run function after `wait` milliseconds have elapsed since last call
  *
- * @name       debounce
- * @tag        Core
- * @signature  (fn: Function, { wait: number, bind: Object }): Function
- *
- * @param {Function} fn                Source function
- * @param {Object}   [props]           Properties
- * @param {number}   [props.wait=50]   Time in milliseconds to wait without calling until invoke
- * @param {Object}   [props.bind=null] `this` provided for the call to `fn`
+ * @param {Function} fn              Source function
+ * @param {Object}   [props]
+ * @param {number}   [props.wait=50] Time in milliseconds to wait without calling until invoke
  *
  * @returns {Function} Wrapper function that calls `fn` after `wait` passed without calling
+ *
+ * @name       debounce
+ * @tag        Core
+ * @signature  (fn: Function, { wait: number }): Function
  *
  * @example
  * const debouncedAutocomplete = debounce(autocompleteFromAPI, {
  *   wait: 100,
- *   bind: this
  * })
  *
  * // render
  * <input onChange={debouncedAutocomplete} ... />
  */
-const debounce = (fn, { wait = 50, bind = null } = {}) => {
+const debounce = (fn, { wait = 50 } = {}) => {
   let finalRunTimer
 
-  return (...args) => {
+  return (...params) => {
     // reset timer at every function call
     clearTimeout(finalRunTimer)
 
     finalRunTimer = setTimeout(() => {
-      fn.apply(bind, args)
+      fn(...params)
     }, wait)
   }
 }
--- src/debounce/debounce.test.js
+++ src/debounce/debounce.test.js
@@ -2,55 +2,67 @@ import test from "tape"
 
 import { debounce } from "./debounce"
 
-test("core::debounce", t => {
-  /**
-   * Debounce with defaults
-   */
-  let defaultCounter = 0
+test("debounce", t => {
+  {
+    let callCount = 0
 
-  let callCount = 0
-  const defaultSet = debounce(source => {
-    callCount = callCount + 1
-    defaultCounter = source
-  })
+    let iteration = 0
 
-  for (let i = 0; i < 100; i++) {
-    defaultSet(i)
+    const defaultSet = debounce(source => {
+      callCount = callCount + 1
+      iteration = source
+    })
+
+    for (let i = 0; i < 100; i++) {
+      defaultSet(i)
+    }
+
+    setTimeout(() => {
+      t.deepEqual(
+        {
+          callCount,
+          i: iteration,
+        },
+        {
+          callCount: 1,
+          i: 99,
+        },
+        "Calling debounce function 100 times should run it once, 50ms after last call"
+      )
+    }, 100)
   }
 
-  setTimeout(() => {
-    t.equal(
-      callCount + defaultCounter,
-      1 + 99,
-      "Calling debounce function 100 times should run it once, 50ms after last call"
+  {
+    let callCount = 0
+
+    let iteration = 0
+
+    const customSet = debounce(
+      source => {
+        callCount = callCount + 1
+        iteration = source
+      },
+      { wait: 100 }
     )
-  }, 100)
 
-  /**
-   * Debounce with custom
-   */
-  let customCounter = 0
+    for (let i = 0; i < 100; i++) {
+      customSet(i)
+    }
 
-  let customCallCount = 0
-  const customSet = debounce(
-    source => {
-      customCallCount = customCallCount + 1
-      customCounter = source
-    },
-    { wait: 100, bind: null }
-  )
+    setTimeout(() => {
+      t.deepEqual(
+        {
+          callCount,
+          i: iteration,
+        },
+        {
+          callCount: 1,
+          i: 99,
+        },
+        "Calling debounced function 100 times with custom timer window"
+      )
 
-  for (let i = 0; i < 100; i++) {
-    customSet(i)
+      t.end()
+    }, 150)
   }
-
-  setTimeout(() => {
-    t.equal(
-      customCallCount + customCounter,
-      1 + 99,
-      "Calling debounced function 100 times with custom timer window"
-    )
-
-    t.end()
-  }, 150)
 })
