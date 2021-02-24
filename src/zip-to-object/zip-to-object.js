b7970808f7690cce697400db87bfcdde58a08504
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2021-02-24 01:06:38 +0100

    feat(zipToObject): Allow curried and uncurried versions

---
+++ src/zip-to-object/zip-to-object.js
@@ -0,0 +1,32 @@
+import { curry } from "../curry/curry"
+
+/**
+ * Create an object from two arrays, one containing keys, the other values.
+ * Both arrays will be trimmed to the smallest length.
+ *
+ * @param {Array} keys
+ * @param {Array} values
+ *
+ * @returns {Object}
+ *
+ * @name zipToObject
+ * @tag Array
+ * @signature (keys: Array) => (values: Array): Object
+ * @signature (keys: Array, values: Array): Object
+ *
+ * @example
+ * zipToObject([a, b])([1, 2]) // => { a: 1, b: 2 }
+ * zipToObject([a], [1, 2]) // => { a: 1 }
+ */
+const zipToObject = curry((keys, values) => {
+  const result = {}
+  const length = Math.min(keys.length, values.length)
+
+  for (let index = 0; index < length; index++) {
+    result[keys[index]] = values[index]
+  }
+
+  return result
+})
+
+export { zipToObject }
--- src/zip-to-object/zip-to-object.test.js
+++ src/zip-to-object/zip-to-object.test.js
@@ -1,21 +1,22 @@
 import test from "tape"
-import { zipToObj } from ".."
 
-test("zipToObj", t => {
+import { zipToObject } from "./zip-to-object"
+
+test("zipToObject", t => {
   t.deepEquals(
-    zipToObj(["a", "b"])([1, 2]),
+    zipToObject(["a", "b"])([1, 2]),
     { a: 1, b: 2 },
     "arrays with same size should form object"
   )
 
   t.deepEquals(
-    zipToObj(["a"])([1, 2]),
+    zipToObject(["a"])([1, 2]),
     { a: 1 },
     "arrays with less keys than values should form object"
   )
 
   t.deepEquals(
-    zipToObj(["a", "b"])([2]),
+    zipToObject(["a", "b"])([2]),
     { a: 2 },
     "arrays with less values than keys should form object"
   )
