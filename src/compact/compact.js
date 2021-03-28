5b5d27d5e693b11ad83d904bdc00a8f2e44ba246
David Gil <dgilperez@gmail.com>
2021-03-28 21:39:56 +0200

    Adds compact (#3)
    feat(compact,compactMany): Add functions to remove "nothing" value from arrays, objects and object arrays

---
+++ src/compact/compact.js
@@ -0,0 +1,73 @@
+import { is } from "../is/is"
+import { map } from "../map/map"
+import { filter } from "../filter/filter"
+import { curry } from "../curry/curry"
+import { type } from "../type/type"
+
+const _compactObject = source => {
+  const result = { ...source }
+  const keys = Object.keys(result)
+
+  for (const key of keys) {
+    if (!is(result[key])) {
+      delete result[key]
+    }
+  }
+
+  return result
+}
+
+const _compactArray = source => filter(is, source)
+
+const _compact = source => {
+  const sourceType = type(source)
+
+  switch (sourceType) {
+    case "Array":
+      return _compactArray(source)
+
+    case "Object":
+      return _compactObject(source)
+
+    default:
+      return source
+  }
+}
+
+const _compactMany = map(_compact)
+
+/**
+ * Returns a copy of the object or array
+ * with all null or undefined values removed
+ *
+ * @param {Array|Object} source
+ *
+ * @returns {Array|Object}
+ *
+ * @name compact
+ * @tag Array,Object
+ * @signature (source: Array|Object): Array|Object
+ *
+ * @see {@link compactMany}
+ * @see {@link is}
+ *
+ * @example
+ * compact([1, null, undefined, {}])
+ * // => [1, {}]
+ *
+ * compact({
+ *   a: "Lorem Ipsum",
+ *   b: null,
+ *   c: undefined,
+ *   d: false,
+ *   f: lambda,
+ *  }),
+ * // => {
+ * //  a: "Lorem Ipsum",
+ * //  d: false,
+ * //  f: lambda
+ * // }
+ */
+export const compact = _compact
+
+export const compactMany = _compactMany
---
+++ src/compact/compact.test.js
@@ -0,0 +1,77 @@
+/* eslint-disable unicorn/no-null */
+import test from "tape"
+
+import { compact, compactMany } from "./compact"
+
+const lambda = () => null
+
+test("compact", t => {
+  t.deepEqual(
+    compact({
+      b: null,
+      c: undefined,
+    }),
+    {},
+    "Compacting an object should return compacted object"
+  )
+
+  t.deepEqual(
+    compact({
+      a: "Lorem Ipsum",
+      b: null,
+      c: undefined,
+      d: false,
+      f: lambda,
+    }),
+    { a: "Lorem Ipsum", d: false, f: lambda },
+    "Compacting an object should return compacted object"
+  )
+
+  t.deepEqual(
+    compact([1, null, undefined, false, "a", lambda]),
+    [1, false, "a", lambda],
+    "Compacting an array should return compacted array"
+  )
+
+  t.deepEqual(compact("Lorem"), "Lorem", "Compacting an string does nothing")
+
+  t.deepEqual(compact(""), "", "Compacting an empty string does nothing")
+
+  t.deepEqual(compact(null), null, "Compacting null does nothing")
+
+  t.deepEqual(compact(), undefined, "Compacting undefined does nothing")
+
+  t.deepEqual(compact(false), false, "Compacting a boolean does nothing")
+
+  t.deepEqual(compact(lambda), lambda, "Compacting a function does nothing")
+
+  t.end()
+})
+
+test("compactMany", t => {
+  t.deepEqual(
+    compactMany([
+      [1, null, undefined, false, "a", lambda],
+      { a: "Lorem Ipsum", b: null, c: undefined, d: false, f: lambda },
+      "Lorem",
+      "",
+      null,
+      undefined,
+      false,
+      lambda,
+    ]),
+    [
+      [1, false, "a", lambda],
+      { a: "Lorem Ipsum", d: false, f: lambda },
+      "Lorem",
+      "",
+      null,
+      undefined,
+      false,
+      lambda,
+    ],
+    "Compacting an array of things compacts each of them"
+  )
+
+  t.end()
+})
